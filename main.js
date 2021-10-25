fe4e068a5756ac2eef941882595c4ea09a775620
Redwan Diriye <ridwanmohamedco@outlook.com>
2021-10-25 07:57:55 -0500

    DONE

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const inputTxt = document.querySelector('.inputTxt');
 var button = document.querySelector('.mainbtn');
 const ShowData = document.querySelector('.ShowData');
+// require('dotenv').config();
 
 // Api Key
 
+// const API_KEY = process.env.MY_API_KEY;
 const API_KEY = "0f957aabcd525cf16c95bb84b6ba5f3d"
 
 button.addEventListener('click', () => {
@@ -24,8 +26,9 @@ button.addEventListener('click', () => {
             //Now Show All Data Value
             ShowData.innerHTML = `
                                 <div class="text-center pt-5 pb-5">
-                                <ul>
+                                <ul style="list-style: none;" >
                                     <h2> ${data.name} </h2> 
+                                    <img src="https://image.flaticon.com/icons/png/512/35/35906.png"  style="width: 200px;" alt="">
                                     <li class="temp">Temp: ${data.main.temp}°F</li>
                                     <li class="tempm">Temp Max: ${data.main.temp_max}°F</li>
                                     <li class="tempm">Temp Min: ${data.main.temp_min}°F</li>
